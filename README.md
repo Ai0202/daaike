816de148d776da2c4cb49a45f86b7f55511c18dc
Atsushi <atsushi88885@gmail.com>
2021-04-29 23:03:28 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## 環境構築手順
+
+## やったことメモ
+- npm i -D eslint
+- npx eslint --init
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+const Home: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen font-mono">
+      daaike
+    </div>
+  )
+}
+export default Home
