01f70fcc592bb5c27c3024d81e0d44b8ac415d6f
Atsushi <atsushi88885@gmail.com>
2021-05-30 01:08:41 +0900

    fixed header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,13 +12,17 @@ const Layout: React.FC<TITLE> = ({ children, title = 'Nextjs' }) => {
       <Head>
         <title>{title}</title>
       </Head>
-      <header>
+      <header className="fixed top-0 right-0 left-0 shadow-md z-50">
         <nav className="bg-neutral-main w-screen">
           <div className="flex justify-between items-center px-8 h-14">
             <div>
               <Link href="/">
                 <a data-testid="home-nav" className="px-3 py-2">
-                  <Image width={60} height={60} src="/images/logo.png" />
+                  <Image
+                    width={48}
+                    height={48}
+                    src="/images/logo_transparent.png"
+                  />
                 </a>
               </Link>
             </div>
@@ -43,7 +47,7 @@ const Layout: React.FC<TITLE> = ({ children, title = 'Nextjs' }) => {
           </div>
         </nav>
       </header>
-      <main className="flex flex-1 justify-center items-center flex-col w-screen">
+      <main className="flex flex-1 justify-center items-center flex-col w-screen mt-14">
         {children}
       </main>
       <Footer />
